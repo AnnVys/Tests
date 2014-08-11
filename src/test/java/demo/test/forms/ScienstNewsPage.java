94679746097ce7dd8e9eab8b54f5d21a43f543f5
AnnVys <dzhambib@gmail.com>
2014-08-11 22:48:19 +0300

    Update ScienstNewsPage.java

--- src/test/java/demo/test/forms/ScienstNewsPage.java
+++ src/test/java/demo/test/forms/ScienstNewsPage.java
@@ -9,7 +9,7 @@ import webdriver.elements.Button;
 public class ScienstNewsPage extends BaseForm {
 	private static String newsLocator="//div[@class='tooltip']/..";
     private Label newsLogo = new Label(By.xpath("//div[@class='m_header']"),"news logo");
-    private Button newsLink = new Button(By.xpath("//ul[@id='newsBlock1']/li[4]/div[2]/a"), "news opener");
+    private Button newsLink = new Button(By.xpath("//ul[@id='newsBlock1']/li[3]/div[2]/a"), "news opener");
 	private Label titleNews = new Label(By.tagName("h1"), "news title");
 
 	public ScienstNewsPage() {
